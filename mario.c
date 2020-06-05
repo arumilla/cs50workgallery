fc18a85fcf12d760b47c221848dc30f57949e043
arumilla <arumilla@users.noreply.github.com>
2020-06-05 22:55:21 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void){
+int main(void)
+{
     int h;
     int c = 0;
     int p = 0;
-    
-    do{
+
+    do
+    {
         h = get_int("height: \n");
-        
+
     } while (!(h > 0 && h <= 8));
-    
-    
-    for (int i = 1; i <= h; i++){
+
+
+    for (int i = 1; i <= h; i++)
+    {
         c = h - i;
         p = i;
-        while (c > 0){
-           
+        while (c > 0)
+        {
+
             printf(" ");
-            
+
             c--;
-            
+
         }
-        
-        while (p > 0){
-           
+
+        while (p > 0)
+        {
+
             printf("#");
-            
+
             p--;
-            
+
         }
-        
+
         printf("\n");
     }
-    
+
 }

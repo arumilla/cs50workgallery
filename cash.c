7aeb162f2d60b6832550d4462fe6fe88a9f43589
arumilla <arumilla@users.noreply.github.com>
2020-06-06 02:30:31 +0000

    automated commit by check50 [check50=True]

---
+++ cash.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int coins = 0;
+    int cents;
+    
+    float owed;
+    do
+    {
+        owed = get_float("Owed: \n");
+    }
+    while(!(owed >= 0));
+    
+    cents = (int)(owed * 100);
+    
+    coins += (cents/25);
+    cents = (cents%25);
+    
+    coins += (cents/10);
+    cents = (cents%10);
+    
+    coins += (cents/5);
+    cents = (cents%5);
+    
+    coins += (cents/1);
+    
+    printf("Coins: %i\n", coins);
+}

069b8c03115ed5a7463c39a6b361e7b0027dc205
arumilla <arumilla@users.noreply.github.com>
2020-06-18 22:15:57 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -34,9 +34,6 @@ int main(int argc, char *argv[])
             inum++;
             
         }
-        else if (img != NULL){
-            fwrite(buffer, 512, 1, img);
-        }
         else if (img != NULL && buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             fclose(img);
@@ -45,6 +42,11 @@ int main(int argc, char *argv[])
             fwrite(buffer, 512, 1, img);
             inum++;
         }
+        else if (img != NULL)
+        {
+            fwrite(buffer, 512, 1, img);
+        }
+        
     }
     fclose(f);
     free(filename);
